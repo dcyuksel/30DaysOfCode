851ab4792a565d5217b41ea6bd230f2ad5d85313
dcyuksel <dnzcnyksl@gmail.com>
2017-09-26 20:33:18 +0300

    Day 5 implemented

---
+++ Day5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day5
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            GetMultiples(n);
+        }
+
+        private static void GetMultiples(int number)
+        {
+            for (var i = 1; i < 11; i++)
+            {
+                Console.WriteLine(@"" + number + " x " + i + " = " +  number * i);
+            }
+        }
+    }
+}
