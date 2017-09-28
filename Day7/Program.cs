7f813fca7771a0fc6a11dcd0ed1f2550d60a554c
dcyuksel <dnzcnyksl@gmail.com>
2017-09-28 21:54:59 +0300

    Day7 implemented

---
+++ Day7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Day7
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var length = Convert.ToInt32(Console.ReadLine());
+            var array = Console.ReadLine().Split(' ');
+
+            for (var i = length - 1; i > 0; i--)
+            {
+                Console.Write(array.ElementAt(i) + " ");
+            }
+
+            Console.Write(array.First());
+        }
+    }
+}
