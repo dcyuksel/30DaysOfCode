e846a3826003025978ef427955c193ed9cf76dd3
DenizCan <dnzcnyksl@gmail.com>
2017-09-30 18:04:57 +0300

    Day9 implemented

---
+++ Day9/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Day9
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(factorial(n));
+        }
+
+        public static int factorial(int n)
+        {
+            if (n == 1)
+            {
+                return n;
+            }
+
+            return n * factorial(n - 1);
+        }
+    }
+}
