5b00fa2cd112e2c99586f3d9f6215c2619fee52e
DenizCan <dnzcnyksl@gmail.com>
2017-10-03 21:49:00 +0300

    day12 implemented

---
+++ Day12/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace Day12
+{
+    using System;
+
+    public class Person
+    {
+        protected string firstName;
+        protected string lastName;
+        protected int id;
+
+        public Person() { }
+        public Person(string firstName, string lastName, int identification)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = identification;
+        }
+        public void printPerson()
+        {
+            Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+        }
+    }
+}
---
+++ Day12/Student.cs
@@ -0,0 +1,50 @@
+﻿namespace Day12
+{
+    using System.Linq;
+
+    public class Student : Person
+    {
+        private int[] testScores;
+
+        public Student(string firstName, string lastName, int identification, int[] testScores)
+        {
+            id = identification;
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.testScores = testScores;
+        }
+
+        public char Calculate()
+        {
+            var averageGrade = GetAverage();
+
+            if (averageGrade<=100 && averageGrade >= 90)
+            {
+                return 'O';
+            }
+            if (averageGrade < 90 && averageGrade >= 80)
+            {
+                return 'E';
+            }
+            if (averageGrade < 80 && averageGrade >= 70)
+            {
+                return 'A';
+            }
+            if (averageGrade < 70 && averageGrade >= 55)
+            {
+                return 'P';
+            }
+            if (averageGrade < 55 && averageGrade >= 40)
+            {
+                return 'D';
+            }
+
+            return 'T';
+        }
+
+        private int GetAverage()
+        {
+            return (int)testScores.Average();
+        }
+    }
+}
